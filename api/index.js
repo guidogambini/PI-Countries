04f9d54faf2baaac99fb140d82a9b81a66ee5e3a
Guido Gambini <guidogambini@hotmail.com>
2021-10-02 12:34:36 -0300

    fixDeploy

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({force: true}).then(() => {
-  server.listen(PORT || 3001, () => {
+  server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
